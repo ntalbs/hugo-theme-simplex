29af89aae931ba405637271c11eead6b1fe2d538
ntalbs <ntalbsen@gmail.com>
2020-02-16 10:21:38 +0000

    Delete semi-colon

--- static/js/mode.js
+++ static/js/mode.js
@@ -35,45 +35,45 @@ function switchMode (mode) {
 }
 
 function makeDark () {
-  root.style.setProperty('--background', "#2a2e2e");
-  root.style.setProperty('--text', "#bbb");
-  root.style.setProperty('--heading', "#eee");
-  root.style.setProperty('--link', "#aaa");
-  root.style.setProperty('--link-hover', "#ccc");
-  root.style.setProperty('--inline-code-bg', "#444");
-  root.style.setProperty('--code-block-bg', "#888");
-  root.style.setProperty('--box-fg', "#aaa");
-  root.style.setProperty('--box-bg', "#3a3a3a");
+  root.style.setProperty('--background', "#2a2e2e")
+  root.style.setProperty('--text', "#bbb")
+  root.style.setProperty('--heading', "#eee")
+  root.style.setProperty('--link', "#aaa")
+  root.style.setProperty('--link-hover', "#ccc")
+  root.style.setProperty('--inline-code-bg', "#444")
+  root.style.setProperty('--code-block-bg', "#888")
+  root.style.setProperty('--box-fg', "#aaa")
+  root.style.setProperty('--box-bg', "#3a3a3a")
 
-  root.style.setProperty('--archive-ul', "#888");
-  root.style.setProperty('--post-meta', "#666");
-  root.style.setProperty('--tagbox', "#888");
+  root.style.setProperty('--archive-ul', "#888")
+  root.style.setProperty('--post-meta', "#666")
+  root.style.setProperty('--tagbox', "#888")
 
-  root.style.setProperty('--line', "#444");
-  root.style.setProperty('--circle', "#444");
-  root.style.setProperty('--footer', "#333");
+  root.style.setProperty('--line', "#444")
+  root.style.setProperty('--circle', "#444")
+  root.style.setProperty('--footer', "#333")
 }
 
 function makeLight () {
-  root.style.setProperty('--background', "#fff");
-  root.style.setProperty('--text', "");
-  root.style.setProperty('--heading', "");
-  root.style.setProperty('--link', "#666");
-  root.style.setProperty('--link-hover', "#999");
-  root.style.setProperty('--inline-code-bg', "#eee");
-  root.style.setProperty('--code-block-bg', "#eee");
-  root.style.setProperty('--box-fg', "#808080");
-  root.style.setProperty('--box-bg', "#f5f5f5");
+  root.style.setProperty('--background', "#fff")
+  root.style.setProperty('--text', "")
+  root.style.setProperty('--heading', "")
+  root.style.setProperty('--link', "#666")
+  root.style.setProperty('--link-hover', "#999")
+  root.style.setProperty('--inline-code-bg', "#eee")
+  root.style.setProperty('--code-block-bg', "#eee")
+  root.style.setProperty('--box-fg', "#808080")
+  root.style.setProperty('--box-bg', "#f5f5f5")
 
-  root.style.setProperty('--archive-ul', "#444");
-  root.style.setProperty('--post-meta', "#aaa");
-  root.style.setProperty('--tagbox', "#555");
+  root.style.setProperty('--archive-ul', "#444")
+  root.style.setProperty('--post-meta', "#aaa")
+  root.style.setProperty('--tagbox', "#555")
 
-  root.style.setProperty('--line', "#ebf2f6");
-  root.style.setProperty('--circle', "#fff");
-  root.style.setProperty('--footer', "#eee");
+  root.style.setProperty('--line', "#ebf2f6")
+  root.style.setProperty('--circle', "#fff")
+  root.style.setProperty('--footer', "#eee")
 }
 
 function resetDisqus() {
-  DISQUS.reset({reload: true});
+  DISQUS.reset({reload: true})
 }
